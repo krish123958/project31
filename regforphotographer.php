4273b82d5de02a35f2dbabcc6784d29b48e410c4
vamsi konakala <60782942+krish123958@users.noreply.github.com>
2021-06-23 17:53:08 +0530

    Update regforphotographer.php

--- regforphotographer.php
+++ regforphotographer.php
@@ -1,9 +1,9 @@
 <?php
     error_reporting(0); 
-    $servername="localhost";
-    $username="root";
-    $password="";
-    $dbname="regdb";
+   $servername="remotemysql.com";
+    $username="voq4GI381Y";
+    $password="1WFM10Kevf";
+    $dbname="voq4GI381Y";
     $n=$p=$cp=$email=$phn='';
     $n=$_POST["name"];
     $p=$_POST["password"];
